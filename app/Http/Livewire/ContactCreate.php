6834766efcd15abb79cf4f55f9940d560fecee7c
denizsetyawan <denizsetyawan95@gmail.com>
2020-09-30 09:53:24 +0700

    nambah validasi

--- app/Http/Livewire/ContactCreate.php
+++ app/Http/Livewire/ContactCreate.php
@@ -17,6 +17,11 @@ class ContactCreate extends Component
     
     public function store()
     {
+        $this->validate([
+            'name' => 'required|min:3',
+            'phone' => 'required|max:15'
+        ]);
+        
         $contact = Contact::create([
             'name' => $this->name,
             'phone' => $this->phone
