6ff48c1a66f051c1c3d4a18dbeef54b805407c9f
denizsetyawan <denizsetyawan95@gmail.com>
2020-09-30 08:46:22 +0700

    menampilkan data

--- resources/views/livewire/contact-index.blade.php
+++ resources/views/livewire/contact-index.blade.php
@@ -1,7 +1,28 @@
 <div>
-    <ul>
-        @foreach ($contact as $c)
-        <li> {{ $c->name }} </li>
-        @endforeach
-    </ul>
+    <table class="table">
+        <thead class="thead-dark">
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Phone</th>
+                <th scope="col"></th>
+            </tr>
+        </thead>
+        <tbody>
+          @php
+          $no=1;
+          @endphp
+           @foreach ($contact as $c)
+            <tr>
+                <th scope="row">{{ $no++ }}</th>
+                <td>{{ $c->name }}</td>
+                <td>{{ $c->phone }}</td>
+                <td>
+                    <button class="btn btn-sm btn-info text-white">Edit</button>
+                    <button class="btn btn-sm btn-danger text-white">Delete</button>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 </div>
